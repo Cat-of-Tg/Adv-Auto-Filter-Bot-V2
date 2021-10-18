591b982161578e19224898c6d84b6efb743e59ae
POISON <86126808+Cat-of-Tg@users.noreply.github.com>
2021-10-18 11:16:58 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -55,14 +55,7 @@ async def start(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
-                update.from_user.first_name),
-        reply_markup=reply_markup,
-        parse_mode="html",
-        reply_to_message_id=update.message_id
-    )
+    
   
     await update.reply_photo(
         chatid_id=update.chat.id,
